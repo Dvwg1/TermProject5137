eb65968b5c22a6f1f504f7523141f688526cfc2b
Daniel-McDougall <103381835+Daniel-McDougall@users.noreply.github.com>
2024-04-24 18:06:36 -0400

    Update crimsonBehelit.py

--- crimsonBehelit.py
+++ crimsonBehelit.py
@@ -20,7 +20,7 @@ def query(prompt):
         test_char = response.choices[0].message.content[0]
         test_2 = responsestring[0]
         sorry_test = responsestring[0:9] #This allows us to check if the program refuses to review the input and retry the sent data
-        while test_char.isascii() != True and test_2.isascii() != True and sorry_test != "I'm sorry" and sorry_test != "Sorry, I ":
+        while test_char.isascii() != True and test_2.isascii() != True and sorry_test != "I'm sorry":
             response = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[{"role": "user", "content": prompt}]
